f8f69d57755be58622a382d0a7b68657b2cd1ddf
AveryClapp <avery.clapp@gmail.com>
2025-12-16 17:52:47 -0500

    my project not claudes

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1,24 +1,10 @@
-#include "server.h"
-#include <iostream>
+// Import CacheAnalyzer here
+//
+//
+#include <stdio.h>
 
-int main(int argc, char* argv[]) {
-    try {
-        // Default port
-        int port = 8080;
+int main(int argc, char **argv) {
 
-        if (argc > 1) {
-            port = std::stoi(argv[1]);
-        }
-
-        std::cout << "Cache Explorer Backend starting on port " << port << "...\n";
-
-        CacheExplorer::Server server(port);
-        server.run();
-
-    } catch (const std::exception& e) {
-        std::cerr << "Fatal error: " << e.what() << "\n";
-        return 1;
-    }
-
-    return 0;
+  // Start server CacheAnalyzer::CacheAnalyzer.run()
+  return 1;
 }
