5afacdf03ab1b442f6aefe5c3a8a0fbd0d1fefec
AveryClapp <avery.clapp@gmail.com>
2026-07-02 15:40:41 -0400

    Harden branch-pattern workload contract

--- examples/branch_patterns.c
+++ examples/branch_patterns.c
@@ -8,21 +8,43 @@
 
 #define N 100000
 
+#if defined(_MSC_VER)
+#define NOINLINE __declspec(noinline)
+#else
+#define NOINLINE __attribute__((noinline))
+#endif
+
+static volatile int always_taken = 1;
+
+static NOINLINE int add_predictable(int total, int i) {
+  return total + (i & 7);
+}
+
+static NOINLINE int add_three(int total) {
+  return total + 3;
+}
+
+static NOINLINE int sub_one(int total) {
+  return total - 1;
+}
+
 int main() {
   volatile int total = 0;
 
 #if RUN_ALTERNATING
   for (int i = 0; i < N; i++) {
     if (i & 1) {
-      total += 3;
+      total = add_three(total);
     } else {
-      total -= 1;
+      total = sub_one(total);
     }
   }
 #else
   for (int i = 0; i < N; i++) {
-    if (i < N - 1) {
-      total += i & 7;
+    if (always_taken) {
+      total = add_predictable(total, i);
+    } else {
+      total = sub_one(total);
     }
   }
 #endif
